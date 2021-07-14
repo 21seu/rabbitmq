f4b8dbf9324944e9fb3592d5b69944f5b882d99b
21seu <1693376841@qq.com>
2021-07-15 07:54:53 +0800

    work模式的消息确认机制

---
+++ 02_work/src/main/java/com/work/Consumer4.java
@@ -0,0 +1,33 @@
+package com.work;
+
+import com.rabbitmq.client.*;
+import com.utils.RabbitMQUtils;
+
+import java.io.IOException;
+
+/**
+ * 消息确认机制
+ * Created by fengtj on 2021/7/15 7:48
+ */
+@SuppressWarnings("all")
+public class Consumer4 {
+
+    public static void main(String[] args) throws IOException {
+
+        Connection connection = RabbitMQUtils.getConnection();
+
+        final Channel channel = connection.createChannel();
+
+        channel.queueDeclare("work", true, false, false, null);
+        //每次只消费一个消息
+        channel.basicQos(1);
+        channel.basicConsume("work", false, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("消费者-4：" + new String(body));
+                //参数1：确认队列中哪个具体消息 一个标识  参数2：是否开启多个消息同时确认
+                channel.basicAck(envelope.getDeliveryTag(),false);
+            }
+        });
+    }
+}
